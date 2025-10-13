dc0e8ea1016f9ab3128c606726c3acff5ef05be3
Sugobet <sugob@qq.com>
2025-10-13 16:16:02 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -245,7 +245,9 @@ def analyze_signature_and_hashes(apk_path):
     except Exception as e:
         print(f"An error occurred during signature analysis: {e}")
 
+
 def main(apk_path):
+    axml_obj = None
     try:
         with zipfile.ZipFile(apk_path, 'r') as apk_zip:
             manifest_data = apk_zip.read('AndroidManifest.xml')
@@ -288,15 +290,51 @@ def main(apk_path):
         print("[*] Conclusion: No high-level security risks were found.")
     print("------------------------------------------------------------")
 
+    return axml_obj
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="A script to scan Android APK files for common vulnerabilities using pyaxmlparser.",
         formatter_class=argparse.RawTextHelpFormatter
     )
     parser.add_argument("apk_path", help="Path to the APK file to be analyzed.")
+    parser.add_argument("s", help="adb device id")
     args = parser.parse_args()
     
-    main(args.apk_path)
+    axml_obj = main(args.apk_path)
 
     import os
-    os.system(f'./ApkCheckPack -f {args.apk_path}')
+    os.system(f'./ApkCheckPack* -f {args.apk_path}')
+
+    # panda or frida-dexdump
+    print_header("Dynamic Analysis:")
+    try:
+        os.system(f'adb -s {args.s} push ./panda-dex-dumper /data/local/tmp')
+        os.system(f'adb -s {args.s} shell "chmod +x /data/local/tmp/panda-dex-dumper"')
+        os.system(f'adb -s {args.s} shell "cd /data/local/tmp/ && /data/local/tmp/panda-dex-dumper -p \$(pidof {axml_obj.get('package')}) &>/dev/null"')
+        os.system(f'adb -s {args.s} pull /data/local/tmp/panda .')
+
+        os.system('mkdir ./jar-result')
+        os.system('cd ./jar-result;for i in `ls ../panda/`;do ../dex-tools-v*/d2j-dex2jar.sh ../panda/$i -f -n --skip-exceptions &>/dev/null;done')
+
+        # jar to java class
+        os.system('jadx -d ./classes ./jar-result/dex_*.jar &>/dev/null')
+
+
+        print_header("SQL query:")
+        os.system("grep -iE 'rawQuery\\(\"|Query\\(\"|execSQL\\(\"' -R ./classes/sources")
+
+        print_header("Root path detection:")
+        os.system("grep -iE '/cache/.disable_magisk|/cache/magisk.log|/cache/su|/data/adb/ksu|/data/adb/ksud|/data/adb/magisk|/data/adb/magisk.db|/data/adb/magisk.img|/data/adb/magisk_simple|/data/local/bin/su|/data/local/su|/data/local/xbin/su|/data/su|/dev/.magisk.unblock|/dev/com.koushikdutta.superuser.daemon/|/dev/su|/init.magisk.rc|/sbin/.magisk|/sbin/su|/su/bin/su|/system/app/Kinguser.apk|/system/app/Superuser.apk|/system/bin/.ext/su|/system/bin/failsafe/su|/system/bin/su|/system/etc/init.d/99SuperSUDaemon|/system/sbin/su|/system/sd/xbin/su|/system/usr/we-need-root/su|/system/xbin/busybox|/system/xbin/daemonsu|/system/xbin/ku.sud|/system/xbin/su|/vendor/bin/su|Kinguser.apk|Superuser.apk|/system/xbin/|/vendor/bin/' -R ./classes/sources")
+
+        print_header("Root package name detection:")
+        os.system("grep -iE 'com.chelpus.lackypatch|com.dimonvideo.luckypatcher|com.koushikdutta.rommanager|com.koushikdutta.rommanager.license|com.koushikdutta.superuser|com.noshufou.android.su|com.noshufou.android.su.elite|com.ramdroid.appquarantine|com.ramdroid.appquarantinepro|com.thirdparty.superuser|com.topjohnwu.magisk|com.yellowes.su|eu.chainfire.supersu|me.weishu.kernelsu|com.kingroot.kinguser|com.kingoapp.root|me.phh.superuser|com.apusapps.browser.module.root|io.github.vvb2060.magisk|com.topjohnwu.magisk.pro|de.robv.android.xposed.installer|org.meowcat.edxposed.manager|me.weishu.exp|com.speedsoftware.rootexplorer|com.keramidas.TitaniumBackup|com.joeykrim.rootcheck|com.device.report|com.qihoo.root|com.dianxinos.optimizer.duplay|com.geohot.towelroot|com.zachspong.temprootremove|com.riru.core|com.github.topjohnwu.magisk.installer|com.alephzain.framaroot|org.chainfire.internet' -R ./classes/sources")
+
+        print_header("Emulator Detection:")
+        os.system("grep -iE 'tel:123456|test-keys|goldfish|android-test|/dev/socket/qemud|/dev/qemu_pipe|/dev/qemu_trace|ro.kernel.qemu|generic_x86|emulator|ro.boot.virtual|ro.cloudbuild.software|ro.secureboot.lockstate|ro.cpu.virtual|Build.PRODUCT=sdk_google|Build.MODEL=Android SDK built|Build.HARDWARE=goldfish|Build.FINGERPRINT=generic|Sensor.TYPE_SIGNIFICANT_MOTION|Sensor.TYPE_STEP_COUNTER|Sensor.TYPE_HEART_RATE|10.0.2.15|eth0|dns.google|debug.stagefright.ccode|ro.kernel.android.checkjni|ro.boot.selinux=disabled|hasQemuSocket|hasQemuPipe|getEmulatorQEMUKernel|Landroid/os/SystemProperties;->get(Ljava/lang/String;)' -R ./classes/sources")
+
+        print_header("Anti-Debugging Detection:")
+        os.system("grep -iE 'checkFridaRunningProcesses|checkRunningProcesses|checkRunningServices|treadCpuTimeNanos|TamperingWithJavaRuntime|com.android.internal.os.ZygoteInit|com.saurik.substrate.MS$2|de.robv.android.xposed.XposedBridge|detectBypassSSL|Landroid/os/Debug;->isDebuggerConnected()Z|:27042|:23946|frida-gadget|libfrida.so|XposedBridge.jar|EdXposed|frida-server|android_server|gdbserver|ro.debuggable|service.adb.root|XposedInstaller|Magisk|LSPosed|ptrace|/proc/self/status|libsubstrate.so|com.saurik.substrate|sslunpinning|JustTrustMe|/data/data/de.robv.android.xposed.installer/conf/modules.list' -R ./classes/sources")
+
+    except:
+        pass
